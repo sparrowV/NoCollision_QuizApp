82f36363781c1e2ae89c854f4428686c139207cb
mishozhghenti <mishozhghenti@gmail.com>
2017-06-09 21:49:37 +0400

    Added Users table (DataBase SQL query)

---
+++ src/SQL/queries.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA IF EXISTS quiz_app;
+CREATE SCHEMA quiz_app;
+
+
+USE quiz_app;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id         INT PRIMARY KEY AUTO_INCREMENT,
+  first_name NVARCHAR(30) NOT NULL,
+  last_name  NVARCHAR(30) NOT NULL,
+  user_name  VARCHAR(20)  NOT NULL,
+  password   VARCHAR(30)  NOT NULL
+);
