e063f3dc4bf6fd85d0e8f5e4f6881ec092d2de9d
Tokoko <togurg14@freeuni.edu.ge>
2017-06-11 12:35:59 +0400

    Add QuestionDAO

--- src/database/DBContract.java
+++ src/database/DBContract.java
@@ -6,7 +6,7 @@ public final class DBContract {
 
 	public static class UserTable {
 		public static final String TABLE_NAME = "users";
-		public static final String COLUMN_NAME_ID = "id";
+		public static final String COLUMN_NAME_ID = "user_id";
 		public static final String COLUMN_NAME_FIRST_NAME = "first_name";
 		public static final String COLUMN_NAME_LAST_NAME = "last_name";
 		public static final String COLUMN_NAME_USERNAME = "username";
--- src/database/bean/Question.java
+++ src/database/bean/Question.java
@@ -1,10 +1,13 @@
 package database.bean;
 
 
+import java.sql.PreparedStatement;
+
 public interface Question {
 
     boolean isCorrect(Answer ans);
     //int getType();
     String toHtml();
 
+    PreparedStatement toSql();
 }
