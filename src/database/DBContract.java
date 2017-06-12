7201a726b5c0ae71a70758a1714ef51d7735e41b
Tokoko <togurg14@freeuni.edu.ge>
2017-06-13 03:34:38 +0400

    Update DBContract

--- src/database/DBContract.java
+++ src/database/DBContract.java
@@ -20,4 +20,43 @@ public final class DBContract {
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_DATA_CREATED = "date_created";
     }
+
+	public static class QuestionQuizTable {
+		public static final String TABLE_NAME = "questions_to_quizzes";
+		public static final String COLUMN_NAME_QUESTION_ID = "question_id";
+		public static final String COLUMN_NAME_TYPE_ID = "type_id";
+		public static final String COLUMN_NAME_QUIZ_ID = "quiz_id";
+	}
+
+	public static class QuestionPlainTable {
+		public static final String TABLE_NAME = "questions_plain";
+		public static final String COLUMN_NAME_QUESTION_ID = "question_id";
+		public static final String COLUMN_NAME_TYPE_ID = "type_id";
+		public static final String COLUMN_NAME_QUESTION = "question";
+	}
+
+	public static class AnswerPlainTable {
+		public static final String TABLE_NAME = "answers_plain";
+		public static final String COLUMN_NAME_ANSWER_ID = "answer_id";
+		public static final String COLUMN_NAME_ANSWER = "answer";
+	}
+
+	public static class AnswerQuestionPlainTable {
+		public static final String TABLE_NAME = "answers_to_questions_plain";
+		public static final String COLUMN_NAME_ANSWER_ID = "answer_id";
+		public static final String COLUMN_NAME_QUESTION_ID = "question_id";
+	}
+
+	public static class QuestionMultipleChoiceTable {
+		public static final String TABLE_NAME = "questions_multchoice";
+		public static final String COLUMN_NAME_QUESTION_ID = "question_id";
+		public static final String COLUMN_NAME_TYPE_ID = "type_id";
+		public static final String COLUMN_NAME_QUESTION = "question";
+		public static final String COLUMN_NAME_CHOICE1 = "choice1";
+		public static final String COLUMN_NAME_CHOICE2 = "choice2";
+		public static final String COLUMN_NAME_CHOICE3 = "choice3";
+		public static final String COLUMN_NAME_CHOICE4 = "choice4";
+		public static final String COLUMN_NAME_CHOICE5 = "choice5";
+		public static final String COLUMN_NAME_ANSWER = "answer";
+	}
 }
