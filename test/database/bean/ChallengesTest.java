c58196b455213d04f0fe4d144128d4d809e05922
Irakli <i@irak.li>
2017-07-17 18:48:56 +0400

    Cleanup, add empty tests (wip)

---
+++ test/database/bean/ChallengesTest.java
@@ -0,0 +1,25 @@
+package database.bean;
+
+import org.junit.Test;
+
+/**
+ * Created by Irakli on 17/07/17.
+ */
+public class ChallengesTest {
+	@Test
+	public void getChallengerID() throws Exception {
+	}
+
+	@Test
+	public void getChallengerUsername() throws Exception {
+	}
+
+	@Test
+	public void getChallengedQuizID() throws Exception {
+	}
+
+	@Test
+	public void getChallengedQuizTitle() throws Exception {
+	}
+
+}
---
+++ test/database/bean/QuizTest.java
@@ -0,0 +1,45 @@
+package database.bean;
+
+import org.junit.Test;
+
+/**
+ * Created by Irakli on 17/07/17.
+ */
+public class QuizTest {
+	@Test
+	public void getIsRandomizedOrder() throws Exception {
+	}
+
+	@Test
+	public void getIsMultiplePages() throws Exception {
+	}
+
+	@Test
+	public void getIsImmediateCorrection() throws Exception {
+	}
+
+	@Test
+	public void getQuizId() throws Exception {
+	}
+
+	@Test
+	public void getAuthorId() throws Exception {
+	}
+
+	@Test
+	public void getTitle() throws Exception {
+	}
+
+	@Test
+	public void getDateCreated() throws Exception {
+	}
+
+	@Test
+	public void getQuestions() throws Exception {
+	}
+
+	@Test
+	public void getCategoryId() throws Exception {
+	}
+
+}
